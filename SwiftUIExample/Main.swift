3f1658722a4770cf5df040e46d2cee062f3a3661
Jyhwenchai <cai5301@qq.com>
2022-01-18 11:06:38 +0800

    using  to start project.

---
+++ SwiftUIExample/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  SwiftUIExample
+//
+//  Created by 蔡志文 on 2022/1/18.
+//  Copyright © 2022 蔡志文. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Main: App {
+    var body: some Scene {
+        WindowGroup {
+            SU_14_SceneStorage()
+        }
+    }
+}
