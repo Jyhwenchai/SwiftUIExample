b7d65959baa4d6afc6dc8100af0f0f7612157234
Jyhwenchai <cai5301@qq.com>
2023-11-24 11:25:01 +0800

    add CustomSlider sample.

---
+++ Swift5.0/CustomSlider/CustomSlider/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  CustomSlider
+//
+//  Created by 蔡志文 on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+          ContentView()
+        }
+    }
+}
